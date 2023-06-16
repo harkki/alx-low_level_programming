f98ffc721b1168d0c082af239d6ac0b308292071
harkki <haroldquarm@gmail.com>
2023-06-15 17:29:55 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that uses the printf function to print a line of code
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
